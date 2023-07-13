9eee4dc19fdefd2e856c7228eba42cf8662fc800
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-13 16:26:40 +0200

    non legge la new date

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 const risultato = document.querySelector(`#risultato`);
-const activate = document.querySelector(".conferma");
+const btnActivate = document.querySelector(".conferma");
 
-const timeOver = new Date (`2023-07-14T09:30:0`).getTime();
+const timeOver = new Date(`2023-07-14T09:30:0`).getTime();
+
+let now;
+btnActivate.addEventListener("click",function(){
+ 
+    const result = idInterval();
+
+    /*console.log(result)
+    console.log(timeOver)*/
+ 
+})
+
+
+function idInterval (/*da_testare*/){
+    
+   now = new Date().getTime(); 
+    let countdown = timeOver - now;
+    
+
+
+
+    console.log(now);
+    console.log(countdown);
+   /* console.log(seconds);
+    console.log(minutes);
+    console.log(hours);*/
+
+
+    return countdown;
+
+}
