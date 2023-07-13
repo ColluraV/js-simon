7f926f835b00ed77344d675faf51cb7e71af5161
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-13 17:38:57 +0200

    timer funzionante, preparativi selezione data

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
 const risultato = document.querySelector(`#risultato`);
 const btnActivate = document.querySelector(".conferma");
 
-const timeOver = new Date(`2023-07-14T09:30:0`).getTime();
+const timeOver = new Date(`2023-07-14T09:30:00`).getTime();
+/*const iTimeOver = new Date(`${iY}-${iM}-${iD}T${iH}:${iMin}:${iS}`).getTime();*/
 
 let now;
 btnActivate.addEventListener("click",function(){
- 
-    const result = timer();
 
-    /*console.log(result)
-    console.log(timeOver)*/
- 
+
+setInterval(function () {
+
+     
+    let result = timer();
+
+
+    console.log(result);
+    console.log(timeOver);
+    
+    const seconds = Math.floor(result / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+
+    console.log(seconds);
+    console.log(minutes);
+    console.log(hours);
+
+    risultato.innerHTML = `${hours} ore ${minutes % 60} minuti ${seconds % 60} secondi`;
+  
+      }, 500);
 })
 
-
-function timer (){/*da_testare*/
+//calcolatore tempo residuo
+function timer (){
     
    now = new Date().getTime(); 
     let countdown = timeOver - now;
-    
-
-
 
     console.log(now);
     console.log(countdown);
-   /* console.log(seconds);
-    console.log(minutes);
-    console.log(hours);*/
-
 
     return countdown;
 
